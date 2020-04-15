2d98c62884d8e813940db7fd59757919a69226a4
marmout pettinelli <orson@marmouts-macbook-pro.home>
2020-04-15 17:06:31 +0200

    fixed the ctrl + left/right arrow feature so that it also recognises the '\n' when met.(happens when editing a line that contains '\n' due to quoted section

--- sources/terminfo_cursor_move3.c
+++ sources/terminfo_cursor_move3.c
@@ -19,15 +19,19 @@ int	terminfo_cursor_move_next_word(t_control *control)
 	int index;
 	int	right_motion;
 	int j;
+	int	current_len;
 
 	j = 0;
 	if (-1 == (index = terminfo_cursor_find_next_word_start(control)))
 		return (1);
 	right_motion = index - control->term->inline_position - 1;
-	control->term->inline_position += right_motion;
 	while (j < right_motion)
 	{
+		current_len = terminfo_predict_current_line_len(control);
 		terminfo_cursor_track_position(control, 1);
+		if (control->term->cursor.x == current_len)
+			terminfo_cursor_move_diagonally_down(control);
+		control->term->inline_position++;
 		j++;
 	}
 	if (!terminfo_cursor_move(control, control->term->cursor.x, \
@@ -38,7 +42,8 @@ int	terminfo_cursor_move_next_word(t_control *control)
 
 /*
 ** note:	this function will find the index of the beginnig of the next word
-**			in the current line we are editing
+**			in the current line we are editing. Whitespaces are considered to
+**			be <space> and <newline>.
 **
 ** RETURN:	-1 not found
 **			else: the index
@@ -53,9 +58,9 @@ int	terminfo_cursor_find_next_word_start(t_control *control)
 	i = control->term->inline_position + 1;
 	while (str[i])
 	{
-		if (str[i] == ' ')
+		if (str[i] == ' ' || str[i] == '\n')
 		{
-			while (str[i] == ' ')
+			while (str[i] && (str[i] == ' ' || str[i] == '\n'))
 				i++;
 			if (str[i])
 				return (i);
@@ -86,10 +91,10 @@ int	terminfo_cursor_move_previous_word(t_control *control)
 	if (-1 == (index = terminfo_cursor_find_previous_word_start(control)))
 		return (1);
 	left_motion = control->term->inline_position + 1 - index;
-	control->term->inline_position -= left_motion;
 	while (j < left_motion)
 	{
 		terminfo_cursor_track_position(control, 0);
+		control->term->inline_position--;
 		j++;
 	}
 	if (!terminfo_cursor_move(control, control->term->cursor.x, \
@@ -117,13 +122,13 @@ int	terminfo_cursor_find_previous_word_start(t_control *control)
 	i = control->term->inline_position;
 	if (i == -1)
 		return (-1);
-	while (str[i] == ' ')
+	while (str[i] && (str[i] == ' ' || str[i] == '\n'))
 		i--;
 	if (i == -1)
 		return (-1);
 	while (i >= 0)
 	{
-		if (str[i] == ' ')
+		if (str[i] == ' ' || str[i] == '\n')
 			return (i + 1);
 		i--;
 	}
