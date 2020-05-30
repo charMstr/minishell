bd4d92975f8c6e6828ec184e1460a739dd097b86
mli <mli@student.42.fr>
2020-05-30 23:20:01 +0200

    builtin env norm + not building if no =

--- sources/builtin_env.c
+++ sources/builtin_env.c
@@ -12,9 +12,9 @@ void	env_builtin(t_list *env)
 {
 	while (env)
 	{
-		ft_putstr_fd(((t_env*)(env->content))->label, 1);
+		ft_putstr_fd(((t_env *)env->content)->label, 1);
 		ft_putstr_fd("=", 1);
-		ft_putendl_fd(((t_env*)(env->content))->value, 1);
+		ft_putendl_fd(((t_env *)env->content)->value, 1);
 		env = env->next;
 	}
 }
@@ -29,21 +29,23 @@ void	env_builtin(t_list *env)
 
 t_list	*env_build_linked_list(char **env)
 {
-	t_list *lst_head;
-	t_list *lst_link;
-	t_env *env_struct;
-	int i;
+	t_list	*lst_head;
+	t_list	*lst_link;
+	t_env	*env_struct;
+	int		i;
 
 	i = 0;
 	lst_head = NULL;
 	while (env[i])
-	{		if (!(env_struct = env_build(env[i])))
+	{
+		if (!(env_struct = env_build(env[i])))
 		{
 			ft_lstclear(&lst_head, env_del_struct);
 			return (NULL);
 		}
 		if (!(lst_link = ft_lstnew(env_struct)))
 		{
+			env_del_struct(env_struct);
 			ft_lstclear(&lst_head, env_del_struct);
 			return (NULL);
 		}
@@ -77,44 +79,32 @@ t_list	*env_build_linked_list(char **env)
 
 void	env_del_struct(void *env)
 {
-	free(((t_env*)env)->label);
-	free(((t_env*)env)->value);
-	free((t_env*)env);
+	free(((t_env *)env)->label);
+	free(((t_env *)env)->value);
+	free((t_env *)env);
 }
 
 /*
 ** note:	This function will get the string either imported from the env, or
 **			sent by the export function. And split it at the first '=' sign
 **			found into two elements of a struct: label and value.
-** note:	If no '=' is found, label takes str, and value is an empty string.
 ** RETURN:	t_env *
 **			NULL if some malloc failed.
 */
 
 t_env	*env_build(char *str)
 {
-	t_env *new;
-	int i;
+	int		i;
+	t_env	*new;
 
-	i = 0;
+	if ((i = ft_strichr(str, '=')) == -1)
+		return (NULL);
 	if (!(new = env_init_struct()))
 		return (NULL);
-	if ((-1 == (i = ft_strichr(str, '='))))
+	if (!(new->label = ft_substr(str, 0, i++)) ||
+		!(new->value = ft_substr(str + i, 0, ft_strlen(str + i))))
 	{
-		if (!(new->label = ft_strdup(str)) || !(new->value = ft_strdup("")))
-		{
-			free(new);
-			return (NULL);
-		}
-		else
-			return (new);
-	}
-	if (!(new->label = ft_substr(str, 0, i)) \
-			|| (!(new->value = ft_substr(str + ++i, 0, ft_strlen(str)))))
-	{
-		free(new->value);
-		free(new->label);
-		free(new);
+		env_del_struct(new);
 		return (NULL);
 	}
 	return (new);
@@ -127,11 +117,11 @@ t_env	*env_build(char *str)
 **			t_env *
 */
 
-t_env *env_init_struct(void)
+t_env	*env_init_struct(void)
 {
-	t_env * new;
+	t_env *new;
 
-	if (!(new = (t_env*)malloc(sizeof(t_env))))
+	if (!(new = (t_env *)malloc(sizeof(t_env))))
 		return (NULL);
 	new->label = NULL;
 	new->value = NULL;
